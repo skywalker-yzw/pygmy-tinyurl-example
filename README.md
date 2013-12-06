7c3503d0e994d23ed77ab7a5eb550384568c4a63
Mark Kockerbeck <mark@kockerbeck.com>
2013-12-05 16:53:10 -0800

    Updating for newer node, express and jade versions

---
+++ README.md
@@ -0,0 +1,9 @@
+# Pygmy
+
+A very basic node.js URL shortener site using the express framework and redis as a repository.
+
+It was started as a learning experience for node.js
+
+```
+node app.js
+```
--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  */
 
 var express = require('express')
-  , app = module.exports = express.createServer()
+  , app = module.exports = express()
   , redis = require('redis').createClient()
   , config = require('./config.js').Config(app, express)
   , shrinkray = require('./modules/shrinkray.js')
@@ -33,4 +33,4 @@ app.get('/:id', MainController.Show);
 // Start it up!
 app.listen(3000);
 
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Express server listening in %s mode", app.settings.env);
